405f9b1808c2f32ec3c77b8a6c1c8825e4f09fa3
massongit <suzukimasaya428@gmail.com>
2018-11-19 00:19:57 +0900

    Add scripts

---
+++ key_phrases.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+# TLから特徴語を抽出し、表示する
+
+require_relative 'config/environment'
+require_relative 'service/key_phrase_service'
+
+key_phrase_service = KeyPhraseService.new
+about_options = { program_name: Plugin::KeyPhrase::Environment::NAME,
+                  version: Plugin::KeyPhrase::Environment::VERSION,
+                  comments: Plugin::KeyPhrase::Environment::DESCRIPTION,
+                  license: begin
+                             path = File.expand_path('LICENSE', __dir__)
+                             file_get_contents(path)
+                           rescue StandardError
+                             nil
+                           end,
+                  website: 'https://github.com/massongit/mikutter_key_phrases',
+                  authors: [Plugin::KeyPhrase::Environment::AUTHOR] }
+
+Plugin.create(:key_phrases) do
+  defactivity(:key_phrases, '特徴語')
+
+  settings('特徴語') do
+    title = 'Yahoo! JAPAN Webサービス用アプリケーションID'
+    inputpass(title, :key_phrases_yahoo_app_id).tooltip(title)
+    title = '取得間隔 (秒)'
+    adjustment(title, :key_phrases_interval, 2, 604_800).tooltip(title)
+    about("#{Plugin::KeyPhrase::Environment::NAME}について", about_options)
+  end
+
+  on_update do |_service, messages|
+    messages.each do |message|
+      key_phrase_service << message[:message] unless message[:message].nil?
+    end
+
+    key_phrases = key_phrase_service.get
+
+    unless key_phrases.empty?
+      output = ['【特徴語】', '　スコア: 単語']
+
+      key_phrases.each do |word, score|
+        output.append(format('　%<score>6d: %<word>s', score: score, word: word))
+      end
+
+      activity(:key_phrases, output.join("\n"))
+    end
+  end
+end
---
+++ service/key_phrase_service.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'json'
+require 'uri'
+
+# 文章中から特徴語を抽出する
+class KeyPhraseService
+  def initialize
+    uri = URI.parse('https://jlp.yahooapis.jp/KeyphraseService/V1/extract')
+    @http = Net::HTTP.new(uri.host, uri.port)
+    @http.use_ssl = true
+    @request = Net::HTTP::Post.new(uri.request_uri)
+    @sentences = []
+    set_next_time
+  end
+
+  def <<(other)
+    s = other.gsub(/^RT +/, '').gsub(/[\w:@+-_.]+/, '')
+    @sentences.append(s.gsub(URI::DEFAULT_PARSER.make_regexp, '').strip)
+  end
+
+  # 文章中から特徴語を抽出する
+  # @return 特徴語一覧
+  def get
+    normalize_sentences
+    key_phrases = {}
+
+    if !@request.body.empty? && (@next_time.nil? || @next_time <= Time.now)
+      unless @next_time.nil?
+        key_phrases = JSON.parse(@http.request(@request).body)
+        @sentences.clear
+      end
+
+      set_next_time
+    end
+
+    key_phrases
+  end
+
+  private
+
+  # リクエストのデータサイズが上限を超えないよう、sentenceの長さを調整する
+  def normalize_sentences
+    app_id = UserConfig[:key_phrases_yahoo_app_id]
+
+    until @sentences.empty?
+      @request.form_data = { appid: app_id,
+                             output: 'json',
+                             sentence: @sentences.join }
+      break if @request.body.length < 102_400
+
+      @sentences.shift
+      app_id = UserConfig[:key_phrases_yahoo_app_id]
+    end
+
+    @request.form_data = {} if app_id.nil? || app_id.empty?
+  end
+
+  # 特徴語の次回取得時刻を設定する
+  def set_next_time
+    interval = UserConfig[:key_phrases_interval]
+    @next_time = if interval.nil?
+                   nil
+                 else
+                   Time.now + interval
+                 end
+  end
+end
